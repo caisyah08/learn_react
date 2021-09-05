5065c8868cdfb6f4d4ab6ac682073dd5d6a9688a
caisyah08 <caisyah0808@gmail.com>
2021-09-05 22:54:27 +0700

    how to pass props from parents to children components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,16 +1,13 @@
 import './ExpenseItem.css'
 
-export default function ExpenseItem() {
-  const expenseDate = new Date(2021,2,28)
-  const expenseAmount = 294.67
-  const expenseTitle = "Car Insurance"
+export default function ExpenseItem({title,date,amount}) {
   return (
     <div className="expense-item"> 
      {/* object tidak bisa langsung dimunculkan dengan bracket kalau di react */}
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
